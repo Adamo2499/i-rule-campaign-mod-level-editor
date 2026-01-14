dd15cb129b160e97f2e8a8c663be32fe32555d91
Adam Bieszk <adambieszk6@gmail.com>
2026-01-14 14:46:58 +0100

    feat(export): added level's JSON code showcase

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 alert('Test!');
+function exportJSON() {
+    const form = document.getElementById("editor");
+    const formData = new FormData(form);
+
+    const json = {};
+
+    for (const [key, value] of formData.entries()) {
+        if (value.trim() === "") continue;   // skip empty fields
+        json[key] = value;
+    }
+
+    document.getElementById("output").textContent =
+        JSON.stringify(json, null, 2);
+}
