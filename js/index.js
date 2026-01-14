9ab7bbd6b90b06a94aae900b925874229a1d9813
Adam Bieszk <adambieszk6@gmail.com>
2026-01-14 14:49:04 +0100

    feat(export): added downloading JSON file functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-alert('Test!');
 function exportJSON() {
     const form = document.getElementById("editor");
     const formData = new FormData(form);
@@ -12,4 +11,30 @@ function exportJSON() {
 
     document.getElementById("output").textContent =
         JSON.stringify(json, null, 2);
+}
+
+function saveJSON() {
+    const form = document.getElementById("editor");
+    const formData = new FormData(form);
+
+    const json = {};
+
+    for (const [key, value] of formData.entries()) {
+        if (value.trim() === "") continue;
+        json[key] = value;
+    }
+
+    const blob = new Blob(
+        [JSON.stringify(json, null, 2)],
+        { type: "application/json" }
+    );
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "level.json";
+    a.click();
+
+    URL.revokeObjectURL(url);
 }
